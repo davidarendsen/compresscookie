a939c7a56efa4e926b03d6f348363d562ed184a0
David Arendsen <davidarendsen@gmail.com>
2016-04-19 22:32:39 +0200

    Added README + Added phpdocs
    Signed-off-by: David Arendsen <davidarendsen@gmail.com>

--- CompressCookie.php
+++ CompressCookie.php
@@ -1,34 +1,60 @@
 <?php
 
+/**
+ * @package CompressCookie
+ * @author David Arendsen
+ */
 class CompressCookie
 {
 
+    /**
+     * @param string $label Set key for the cookie
+     * @param mixed|bool $content The content for the cookie
+     */
     public function setcookie($label, $content)
     {
         setcookie($label, $this->compress($content), time()+3600, '/');
     }
 
+    /**
+     * @param string $label Get cookie by this key
+     * @return mixed|bool Uncompressed content of cookie
+     */
     public function getcookie($label)
     {
-        return $this->uncompress($_COOKIE[$label]);
+        if ( isset($_COOKIE[$label]) ) {
+            return $this->uncompress($_COOKIE[$label]);
+        }
+        else {
+            return false;
+        }
     }
 
+    /**
+     * @param string $content Compress this content
+     * @param bool $jsonEncode If the content has to be JSON encoded or serialized
+     * @return string Compressed content (base64 encoded)
+     */
     public function compress($content, $jsonEncode = true)
     {
         if ( is_array($content) or is_object($content) ) {
             if ($jsonEncode) {
-                $test = json_encode($content);
+                $serialized = json_encode($content);
             }
             else {
-                $test = serialize($content);
+                $serialized = serialize($content);
             }
-            return base64_encode( gzcompress( $test ) );
+            return base64_encode( gzcompress( $serialized ) );
         }
         else {
             return base64_encode( gzcompress($content) );
         }
     }
 
+    /**
+     * @param string $content Compress this content
+     * @return string|array|object Uncompressed content
+     */
     public function uncompress($content)
     {
         $uncompressed = gzuncompress( base64_decode($content) );
@@ -36,7 +62,7 @@ class CompressCookie
         if ( @unserialize($uncompressed) ) {
             return unserialize($uncompressed);
         }
-        elseif ( @json_decode($uncompressed, true) ) {
+        elseif ( @json_decode($uncompressed) ) {
             return json_decode($uncompressed);
         }
         else {
